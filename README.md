adb6bbf61ef3e1e87d0f6cfdc192071d03d06857
Gustavo Robson <gustavo.silva510@fatec.sp.gov.br>
2024-12-24 14:58:23 -0300

    loja 0.1

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# loja-virtual
+<p align="center">
+<img align="auto" width="60px" src="/assets/img/logo_icongoku.png">
+</p>
+<h1 align="center"> Projeto em grupo - Loja Virtual Goku</h1>
+<p align="center">Projeto proposto pelo professor Bruno para Desenvolvimento de uma Loja Virtual do Dragon Ball. 
+<br>
+<strong> Desenvolvido por Alice Laila, Gustavo Robson, Nycole Oliveira e Janaina Mota.</strong></p>
+<br>
+<div align="center">
+  
+   [Página de login](página-de-login) • [Home](#home) • [Sobre nós](#sobre-nós) • [Carrinho](#carrinho)
+   
+</div>
+
+<p><b> Link da plataforma:https://loja-virtual-goku.vercel.app/</b></p>
+
+<strong> Login: snoopy@gmail.com 
+<br>
+Senha: 1950
+</strong>
+
+## Página de login
+
+![Login](https://github.com/user-attachments/assets/50d64992-b26b-4095-aa7a-90196d7ba71e)
+
+## Cadastro
+
+![cadastro](https://github.com/user-attachments/assets/76b4b217-20bc-46c7-a6f5-4fcba1fb0116)
+
+## Home 
+![home](https://github.com/user-attachments/assets/1581e4fa-98b4-4959-994a-cc2d57066807)
+
+## Sobre nós 
+
+![sobrenos2](https://github.com/user-attachments/assets/342373e5-068a-4b3d-a6d3-493a593b6722)
+
+## Carrinho 
+
+![Carrinho](https://github.com/user-attachments/assets/b93abab1-8144-4814-b3f7-b24f53cfbee9)
+
---
+++ pages/login/login.js
@@ -0,0 +1,94 @@
+function local(){
+
+  if (localStorage.getItem('usersHere') === null) {
+      const ds = [ 
+          { id: 1, login: "Alice", password: "1234", email: "alice@gmail.com"},//[0]
+          { id: 2, login: "Gustavo", password: "12345@", email: "gustavo@gmail.com"},//[1]
+          { id: 3, login: "Janaina", password: "123456@", email: "janaina@gmail.com"},//[2]
+          { id: 4, login: "snoopy", password: "1950", email: "snoopy@gmail.com"}
+       ]
+      let n = JSON.stringify(ds);
+      localStorage.setItem("usersHere", n);   
+      return ds  
+  } 
+
+}
+
+function doLogin(event){
+  event.preventDefault();
+
+  const usuarios = JSON.parse(localStorage.getItem("usersHere"))
+  
+  let log = document.querySelector("#email").value
+  let senha = document.querySelector('#password').value
+
+      for(let i = 0; i < usuarios.length; i++){
+          if((log == usuarios[i].email) && senha == usuarios[i].password){
+              let n = JSON.stringify(usuarios[i]);
+              sessionStorage.setItem("usersHere", n);
+              alert(`${usuarios[i].login} logou`);
+              window.location.href = ('/');
+              usuarioEncontrado = true;
+              break;
+          }
+        }
+        if (!usuarioEncontrado) {
+          alert(`Usuario não cadastrado`);
+        }
+}
+
+function cadastrar(event) {
+  event.preventDefault();
+
+  var usuarios = JSON.parse(localStorage.getItem("usersHere"))
+  let nome = document.querySelector("#nome1").value
+  let senha = document.querySelector("#password1").value
+  let email = document.querySelector("#email1").value
+
+  if (nome !== '' && senha !== '' && email !== ''){
+      let user = { id: Date.now(), login: nome, password: senha, email:email}
+      usuarios.push(user)
+      localStorage.setItem("usersHere", JSON.stringify(usuarios))
+      alert('Conta Registrada!')
+      window.location.href = ("/pages/login/login.html")
+  
+  }
+
+}
+
+function logout() {
+  sessionStorage.removeItem("usersHere");  // Remove as informações do usuário do sessionStorage
+  window.location.href = ("/")  // Redireciona para a página inicial ou outra desejada
+}
+
+function setUserType(val){
+  n = JSON.stringify(val);
+  sessionStorage.setItem("userType", n)
+
+}
+function esconderLinksDeLogin() {
+  // Verifica se o usuário está logado, ou seja, se o item 'userHere' existe no sessionStorage
+  const user = sessionStorage.getItem('usersHere');
+
+  // Se o usuário estiver logado (se 'userHere' existir no sessionStorage)
+  if (user) {
+    const loginLink = document.getElementById('homeLink');  // Encontra o link de 'Home'
+    const logoutItem = document.getElementById('logoutItem');
+
+    // Verifica se o link 'Home' realmente existe na página antes de tentar escondê-lo
+    if (loginLink) {
+      loginLink.style.display = 'none';  // Esconde o link de 'Home'
+    }
+    if(logoutItem){
+      logoutItem.style.display = 'block';
+    }
+  }else {
+    if (loginLink) {
+      loginLink.style.display = 'block';  // Garante que o link de 'Login' seja visível se não estiver logado
+    }
+
+    if (logoutItem) {
+      logoutItem.style.display = 'none';  // Garante que o botão de 'Logout' seja oculto se não estiver logado
+    }
+  }
+}
